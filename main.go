135d2ea90532f02517845caa3d289979d7a5c64b
lizhiqiang <13482477@qq.com>
2018-02-20 20:05:35 +0800

    commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.File("/", "webapp/index.html")
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
